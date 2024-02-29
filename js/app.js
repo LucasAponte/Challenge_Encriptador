791ab5dd4e645d0f5b9d1636c6bc638557920d24
Aponte Lucas <Aponte_Lucas_g@hotmail.com>
2024-02-29 16:21:18 -0300

    Carga inicial
    Se cargan lo archivos, tanto como los códigos como las imágenes a usar.

---
+++ js/app.js
@@ -0,0 +1,59 @@
+const texto_entrante = document.querySelector(".texto_ingresado");
+
+
+
+function asignarTexto(texto) {
+    let elementoHTML = document.querySelector(".salida textarea");
+    elementoHTML.innerHTML = texto;
+    elementoHTML.style.textAlign = 'left';
+    elementoHTML.style.marginTop = '4.5em';
+    elementoHTML.style.color = '#495057'
+    elementoHTML.style.fontWeight = 'normal';
+    elementoHTML.rows = 10;
+    elementoHTML.cols = 33;
+}
+
+function btn_encriptar() {
+    const texto_encriptado = encriptador(texto_entrante.value, true);
+    ocultar();
+    asignarTexto(texto_encriptado);
+}
+
+function btn_desencriptar() {
+    const texto_desencriptado = encriptador(texto_entrante.value);
+    ocultar();
+    asignarTexto(texto_desencriptado, false);
+}
+
+function btn_copiar() {
+    let copyText = document.querySelector(".salida textarea");
+    copyText.select();
+    document.execCommand("copy");
+    alert("El texto fue copiado")
+  }
+
+function ocultar() {
+    let elemento = document.querySelector(".salida h6")
+    elemento.style.display = 'none';
+    elemento = document.querySelector(".salida img")
+    elemento.style.display = 'none';
+}
+
+function encriptador(texto, bool) {
+    let matriz_codigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    texto = texto.toLowerCase();
+    if (bool) {
+        for (let i = 0; i < matriz_codigo.length; i++) {
+            if (texto.includes(matriz_codigo[i][0])) {
+                texto = texto.replaceAll(matriz_codigo[i][0], matriz_codigo[i][1]);
+            }
+        }
+    } else {
+        for (let i = 0; i < matriz_codigo.length; i++) {
+            if (texto.includes(matriz_codigo[i][1])) {
+                texto = texto.replaceAll(matriz_codigo[i][1], matriz_codigo[i][0]);
+            }
+        }
+    }
+    return texto;
+}
